7e1478d329879f39f53bb471f62092dd97cb0b42
Ryan <ry.huey849@gmail.com>
2019-09-24 20:31:16 -0700

    set up branch

--- server/models/image-model.js
+++ server/models/image-model.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+mongoose.connect('mongodb://127.0.0.1:27017/poseApp')
+
 const Image = new Schema(
   {
     img: {data: Buffer, contentType: String} //data type is Buffer so that we can store our image as data in form of array
@@ -9,3 +11,5 @@ const Image = new Schema(
 )
 
 module.exports = mongoose.model('uploads', Image)
+
+pose: {leftAnkle: Float, rightAnkle: Float}
