087f9a6425ecffb04192bb425bced987ebbec24a
Byeongsoo Kwon <dot567@naver.com>
2019-10-15 11:31:58 -0700

    Update image-model.js

--- server/models/image-model.js
+++ server/models/image-model.js
@@ -1,7 +1,7 @@
+//database that creates the schema
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-
-mongoose.connect('mongodb://127.0.0.1:27017/cinema', {useNewUrlParser: true})
+mongoose.connect('mongodb://127.0.0.1:27017/cinema')
 
 const Image = new Schema(
   {
@@ -10,4 +10,11 @@ const Image = new Schema(
   {timestamps: true},
 )
 
+const PImage = new Schema(
+  {
+    img: {data: Buffer, contentType: String} //data type is Buffer so that we can store our image as data in form of array
+  },
+  {timestamps: true},
+)
 module.exports = mongoose.model('uploads', Image)
+module.exports = mongoose.model('pose', PImage)
