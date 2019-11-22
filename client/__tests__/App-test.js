0bf5df8ba7310e7d15a074123b5c080a1847f97d
Ryan <ry.huey849@gmail.com>
2019-11-21 16:58:03 -0800

    Login tests with Jest/Enzyme
    Checks ability to write to email and password fields, correct login, wrong email and correct password, vice versa.

--- client/__tests__/App-test.js
+++ client/__tests__/App-test.js
@@ -1,14 +1,39 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
-import App from '../App';
+import { shallow } from 'enzyme';
+import Login from './screens';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+describe('Test case for testing login',() =>{
+  let wrapper;
+  test('email input check',()=>
+  {
+    wrapper = shallow(<Login/>);
+    wrapper.find('input[type="text"]').simulate('change', {target: {name: 'email', value: 'tobiguthix@gmail.com'}});
+    expect(wrapper.state('email')).toEqual('tobiguthix@gmail.com');
+  })
+  it('password input check',()=>{
+    wrapper = shallow(<Login/>);
+    wrapper.find('input[type="password"]').simulate('change', {target: {name: 'password', value: 'password123'}});
+    expect(wrapper.state('password')).toEqual('password123');
+  })
+  it('checks wrong email right pass',()=>{
+    wrapper = shallow(<Login/>);
+    wrapper.find('input[type="text"]').simulate('change', {target: {name: 'email', value: 'tix@gmail.com'}});
+    wrapper.find('input[type="password"]').simulate('change', {target: {name: 'password', value: 'test123'}});
+    wrapper.find('button').simulate('click');
+    expect(wrapper.state('isLoggedIn')).toBe(false);
+  })
+  it('checks right email wrong pass',()=>{
+    wrapper = shallow(<Login/>);
+    wrapper.find('input[type="text"]').simulate('change', {target: {name: 'email', value: 'tobiguthix@gmail.com'}});
+    wrapper.find('input[type="password"]').simulate('change', {target: {name: 'password', value: '8a8sdahikkk  **j'}});
+    wrapper.find('button').simulate('click');
+    expect(wrapper.state('isLoggedIn')).toBe(false);
+  })
+  it('checks valid sign in',()=>{
+    wrapper = shallow(<Login/>);
+    wrapper.find('input[type="text"]').simulate('change', {target: {name: 'email', value: 'tobiguthix@gmail.com'}});
+    wrapper.find('input[type="password"]').simulate('change', {target: {name: 'password', value: 'test123'}});
+    wrapper.find('button').simulate('click');
+    expect(wrapper.state('isLoggedIn')).toBe(true);
+})
